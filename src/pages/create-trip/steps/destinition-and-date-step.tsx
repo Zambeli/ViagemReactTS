70df433cada96750b2cf750c25283e55b21c0513
Zambeli <zambelidev@gmail.com>
2024-07-12 22:23:20 -0300

    terminando 2 parte projeto

--- src/pages/create-trip/steps/destinition-and-date-step.tsx
+++ src/pages/create-trip/steps/destinition-and-date-step.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface DestinationAndDateStep {
   isGuestsInputOpen: boolean
@@ -26,14 +27,15 @@ export function DestinationAndDateStep({
       <div className='w-px h-6 bg-zinc-400' />
 
       {isGuestsInputOpen ? (
-        <button onClick={closeGuestsInput} className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>Alterar lugar/data
-          <Settings2 className='size-5' />
-        </button>
+        <Button  onClick={closeGuestsInput} variant="secondary">
+            Alterar lugar/data
+            <Settings2 className='size-5' />
+        </Button>
       ) : (
-        <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-          Continuar
-          <ArrowRight className='size-5' />
-        </button>
+        <Button  onClick={openGuestsInput}  variant="primary">
+            Continuar
+            <ArrowRight className='size-5' />
+        </Button>
       )}
     </div>
   )
--- src/pages/create-trip/steps/invite-guests-step.tsx
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, UserRoundPlus } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface InviteGuestsStepProps {
   openGuestsModal: () => void
@@ -24,10 +25,12 @@ export function InviteGuestsStep({
 
       <div className='w-px h-6 bg-zinc-400' />
 
-      <button onClick={openConfirmTripModal} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-        Confirmar Viagem
-        <ArrowRight className='size-5' />
-      </button>
+      <Button onClick={openConfirmTripModal} variant="primary">
+             Confirmar Viagem
+             <ArrowRight className='size-5' />
+      </Button>
+
+
     </div>
   )
 }
